b7d41690eea92b21880a92e16558117d9cf2ef53
Justin Duong <jduong@umich.edu>
2022-06-22 21:07:10 -0400

    Removed client id.

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.listen(PORT, () => {
 
 app.get('/authorize', (req, res) => {
   res.send({
-    'redirectLink': 'https://www.reddit.com/api/v1/authorize?client_id=Z4DY5cYfHe2YN2H8MrytJg&response_type=code&state=hey&redirect_uri=http://localhost:5000/getToken&duration=temporary&scope=identity'
+    'redirectLink': 'https://www.reddit.com/api/v1/authorize?client_id=CLIENT_ID&response_type=code&state=hey&redirect_uri=http://localhost:5000/getToken&duration=temporary&scope=identity'
   });
 });
 
